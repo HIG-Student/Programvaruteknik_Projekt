644b9bd657de5579e73c0c9b7e5389d15facca2a
Hampus <ntn13hcm@12892-STUD-D.HIG-AD.hig.se>
2016-04-12 14:05:49 +0200

    Fixat med korrelation mellan två datakällor(punkt 2 klar)
    This resolves #1

--- Programvaruteknik_Projekt/WebContent/app/graphs/corr_view/corr_view.ts
+++ Programvaruteknik_Projekt/WebContent/app/graphs/corr_view/corr_view.ts
@@ -1,12 +1,88 @@
-import {Component} from "angular2/core";
+import {Component, NgZone, AfterView, OnDestroy, Input} from "angular2/core";
+import {DataLoader} from "app/data_loader";
+
+class Chart
+{ 
+	id: String;
+	data: Object;
+	height: any;
+	width: any;
+	constructor(config: Object) 
+	{
+		this.id = config['id'];
+		this.data = config['data'];
+		this.height = config['height'] || 300;
+		this.width = config['width'] || 600;
+	}
+}
+
+@Component(
+{
+	selector: 'zingchart',
+	inputs: ['chart'],
+	template: "<div id='{{chart.id}}'></div>"
+})
+class ZingChart implements AfterView, OnDestroy
+{
+	chart: Chart;
+	constructor(private zone: NgZone)
+	{}
+
+	ngAfterViewInit()
+	{
+		this.zone.runOutsideAngular(() =>
+		{
+			zingchart.render(
+			{
+				id: this.chart['id'],
+				data: this.chart['data'],
+				width: this.chart['width'],
+				height: this.chart['height']
+			});
+		});
+	}
+	ngOnDestroy()
+	{
+		zingchart.exec(this.chart['id'], 'destroy');
+	}
+}
 
 
-@Component({
+
+@Component(
+{
 	selector: "corr-view",
-	templateUrl: "app/graphs/corr_view/corr_view.html",
-	styleUrls: ["app/graphs/corr_view/corr_view.css"]
+	directives: [ZingChart],
+	templateUrl: "app/graphs/corr_view/corr_view.html"
 })
 export class CorrView 
 {
-	constructor() { }
+	@Input("chart-id") id: string;
+	
+	charts: Chart[];
+	
+	constructor(private dataLoader: DataLoader) { }
+	
+	ngOnInit()
+	{
+		this.name = 'Angular2'
+		
+		this.dataLoader.getDataSource().subscribe(data =>
+		{
+			this.charts = 
+			[{
+				id: "chart-" + this.id,
+				data:
+				{
+					type: "scatter",
+					series: 
+					[{
+						values: Object.keys(data.data).map(k=>[data.data[k].a,data.data[k].b])
+					}],
+				},
+				height: 400,
+				width: 600
+			}]
+		});
+	}
 }
