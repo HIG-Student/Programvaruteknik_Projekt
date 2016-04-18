597a7d2a84089c934256d89547221c8941af734f
ofk14mbi <ofk14mbi@07609-STUD-D.HIG-AD.hig.se>
2016-04-18 15:47:35 +0200

    Sourcebuilder nästan klar

--- Programvaruteknik_Projekt/src/se/hig/programvaruteknik/data/QuandlDataSourceBuilder.java
+++ Programvaruteknik_Projekt/src/se/hig/programvaruteknik/data/QuandlDataSourceBuilder.java
@@ -11,27 +11,36 @@ import se.hig.programvaruteknik.model.DataSource;
 
 public class QuandlDataSourceBuilder extends JSONDataSourceBuilder 
 {
-	private String sourceName;
 	
 	public static enum SourceType 
 	{
-		DEATH_RATE_CIRRHOSIS("Cirrhosis", "Rate", "Death rate from liver cirrhosis",
+		DEATH_RATE_CIRRHOSIS_USA("Cirrhosis", "Rate", "Death rate from liver cirrhosis",
 				"https://www.quandl.com/api/v3/datasets/NIAAA/CIRRHOSIS.json"),
 		
-		US_UNEMPLOYMENT_RATE("Unemployment", "Rate", "Unemployment rate in USA",
+		UNEMPLOYMENT_RATE_USA("Unemployment", "Rate", "Unemployment rate in USA",
 				"https://www.quandl.com/api/v3/datasets/FRED/LNU03000000.json"),
+		
+		CRIME_RATE_USA("Crime rate in USA", "Rate", "USA",
+				"https://www.quandl.com/api/v3/datasets/FBI_UCR/USCRIME_TYPE_VIOLENTCRIMERATE.json"),
+		
+		ALCOHOL_CONSUMPTION_USA("Alcohol consumption", "%", "Percentage of population in USA consuming alcohol",
+				"https://www.quandl.com/api/v3/datasets/NIAAA/DRINKERS.json"),
 
-		GAS_AND_DIESEL_PROD("Production", "Metric tons - thousand", "Gas and diesel production in Sweden",
+		GAS_AND_DIESEL_PROD_SWE("Production", "Metric tons - thousand", "Gas and diesel production in Sweden",
 				"https://www.quandl.com/api/v3/datasets/UN/GASDIESELOILSTOTALPRODUCTION_SWE.json"),
 		
-		GAS_AND_DIESEL_STOCK("Price variance", "$", "Price variance of gas and diesel in Sweden",
+		GAS_AND_DIESEL_STOCK_SWE("Price variance", "$", "Price variance of gas and diesel in Sweden",
 				"https://www.quandl.com/api/v3/datasets/UN/GASDIESELOILSCHANGESINSTOCKS_SWE.json"),
 		
 		ENROLMENT_EDU_SWE("Education enrolment", "Number", "Enrolment of women in education",
 				"https://www.quandl.com/api/v3/datasets/UGEN/ENRL_SWE.json"),
 		
-		US_CRIME_RATE("Crime rate in USA", "Rate", "USA",
-				"https://www.quandl.com/api/v3/datasets/FBI_UCR/USCRIME_TYPE_VIOLENTCRIMERATE.json");
+		EMPLOYMENT_RATE_FEMALE_SWE("Employment", "%", "Employment of women from the age of 15+",
+				"https://www.quandl.com/api/v3/datasets/UGEN/EMPL_SWE.json"),
+		
+		EMPLOYMENT_RATE_OVERALL_SWE("Employment", "Value", "Employment ",
+				"https://www.quandl.com/api/v3/datasets/ODA/SWE_LE.json");
+		
 		
 		private String name;
 		private String unit;
@@ -74,16 +83,9 @@ public class QuandlDataSourceBuilder extends JSONDataSourceBuilder
 	setSourceLink("http://www.quandl.com");
     }
 
-    /**
-     * Create a stock-source-builder
-     * 
-     * @param info
-     *            The information that we should get about the stock
-     */
     public QuandlDataSourceBuilder(SourceType source)
     {
 	    this();
-		setSourceType(source);
 		setSourceSupplier(DataSupplierFactory.createURLFetcher(source.getUrl()));
 		setListExtractor(
 				(data) -> listToMap((((List<List<Object>>) ((Map<String, Object>) data.get("dataset")).get("data")))));
@@ -92,12 +94,6 @@ public class QuandlDataSourceBuilder extends JSONDataSourceBuilder
 		setNameExtractor((data) -> ((Map<String, Object>) data.get("dataset")).get("name").toString().split(",")[0]);
 		setUnit(source.getUnit());
     }
-
-	private void setSourceType(SourceType source) 
-	{
-		// TODO Auto-generated method stub
-		
-	}
 	
 	@SuppressWarnings("serial")
 	public List<Map<String, Object>> listToMap(List<List<Object>> list) {
@@ -110,7 +106,7 @@ public class QuandlDataSourceBuilder extends JSONDataSourceBuilder
 	}
 
 	public static void main(String[] args) {
-		QuandlDataSourceBuilder build = new QuandlDataSourceBuilder(SourceType.DEATH_RATE_CIRRHOSIS);
+		QuandlDataSourceBuilder build = new QuandlDataSourceBuilder(SourceType.GAS_AND_DIESEL_STOCK_SWE);
 		DataSource source = build.build();
 		for (Entry<LocalDate, Double> entry : source.getData().entrySet()) {
 			System.out.println(entry.getKey() + ": " + entry.getValue());
