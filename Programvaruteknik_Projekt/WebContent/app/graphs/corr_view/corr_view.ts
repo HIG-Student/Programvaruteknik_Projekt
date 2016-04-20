14427b994803573b499d58f4a9c5948b3ca5197a
ntn13dcm <ntn13dcm@07586-STUD-D.HIG-AD.hig.se>
2016-04-20 15:16:12 +0200

    Regressionslinje "klar" se detaljer.
    Våran egna ritar fel vid några värden(grå linje), så nu är Zingcharts
    egna drawtrendline(röd linje) inlagd till koden. Blir dock ett "uncaught
    error" but it works!

--- Programvaruteknik_Projekt/WebContent/app/graphs/corr_view/corr_view.ts
+++ Programvaruteknik_Projekt/WebContent/app/graphs/corr_view/corr_view.ts
@@ -37,6 +37,13 @@ class ZingChart implements AfterView, OnDestroy
 				data: this.chart['data'],
 				width: this.chart['width'],
 				height: this.chart['height']
+				events:
+				{
+     			   load:function()
+     			   {
+            		$("#chart-corr").drawTrendline();
+        			}
+    			}
 			});
 		});
 	}
@@ -62,13 +69,120 @@ export class CorrView
 	
 	constructor() { }
 	
+	calculate(xyValues)
+	{
+		var xValues = [xyValues.length];
+		var yValues = [xyValues.length];
+	
+		for(var i=0; i<xyValues.length;i++)
+		{
+			xValues[i]=xyValues[i][0];
+			yValues[i]=xyValues[i][1];
+		}	
+
+		var nbrOfElements = xyValues.length;
+		var xSum = getSumOf(xValues);
+		var ySum = getSumOf(yValues);
+		var xySum = getSumOfArrayNodeProducts(xValues, yValues);
+		var xSquaredSum = getSumOfArrayNodeProducts(xValues, xValues);
+		var ySquaredSum = getSumOfArrayNodeProducts(yValues, yValues);
+	
+		function getSumOfArrayNodeProducts(aValues,bValues)
+		{
+			var sum = 0;
+			for (var i = 0; i < nbrOfElements; i++) 
+			{
+				sum += aValues[i] * bValues[i];
+			}
+			return sum;
+		}
+		
+		function getSumOf(values) 
+		{
+			var sum = 0;
+			for (var i = 0; i < values.length; i++) 
+			{
+				sum += values[i];
+			}
+			return sum;
+		}
+		
+		function getKValue()
+		{
+			return ((nbrOfElements*xySum)-(xSum*ySum))/((nbrOfElements*xSquaredSum)-(xSum*xSum));
+		}
+		
+		function getMValue()
+		{
+			return ((xSquaredSum*ySum)-(xSum*xySum))/
+				((nbrOfElements*xSquaredSum)-(xSum*xSum));
+		}
+		
+		function getRValue() 
+		{
+			return (Math.pow(((nbrOfElements*xySum)-(xSum*ySum)), 2))/
+				(((nbrOfElements*xSquaredSum)-(xSum*xSum))*((nbrOfElements*ySquaredSum)-(ySum*ySum)));
+		}
+		
+		function getRegLineMinYValue() 
+		{
+			return straightLineEqFofX(getMinX());
+		}
+
+		function getRegLineMaxYValue() 
+		{
+			return straightLineEqFofX(getMaxXValue());
+		}
+		
+		function straightLineEq( x )
+		{
+			return (getKValue()*x)+getMValue();
+		}
+
+		function getMinX() 
+		{
+			var minX = null;
+			for (var i = 0; i < xValues.length; i++) 
+			{
+				if (minX == null || minX > xValues[i])
+					minX = xValues[i];
+			}
+			return minX;
+		}
+
+		function getMaxX()
+		{
+			var maxX = null;
+			for (var i = 0; i < xValues.length; i++) 
+			{
+				if (maxX == null || maxX < xValues[i])
+					maxX = xValues[i];
+			}
+			return maxX;
+		}
+		
+		return {
+			"m": getMValue(),
+			"k": getKValue(),
+			"R": getRValue(),
+			"Y1": straightLineEq(getMinX()),
+			"Y2": straightLineEq(getMaxX()),
+		}
+	}
+	
 	update(data:object)
 	{
+		var info = this.calculate(Object.keys(data.data).map(k=>[data.data[k].a,data.data[k].b]));
+		
+		window.info = info;
+		
 		this.charts = 
 		[{
+		
 			id: "chart-" + this.id,
 			data:
-			{
+			{	
+			
 				type: "scatter",
 				"scale-x":
 				{  
@@ -82,8 +196,20 @@ export class CorrView
 					label:
 					{  
 						text: data.b_name + " (" + data.b_unit + ")"
-					}
+					},
+					"markers": 
+					[
+			            {
+			            
+			                "type": "line",
+			                "range": [info.Y1, info.Y2],
+			                "line-style": "dashed"
+			                
+			            }
+			        ]
+			        
 				},
+				
 				plotarea:
 				{
 					margin: "75px"
@@ -109,6 +235,7 @@ export class CorrView
 			height: 400,
 			width: 600
 		}];
+		
 	}
 	
 	ngOnInit()
@@ -120,6 +247,7 @@ export class CorrView
 	{
 		this.charts = 
 		[{
+			
 			id: "chart-" + this.id,
 			data:
 			{
